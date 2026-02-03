47aac656cd7ed771b99f70b372e41ade00bf49ec
shinano <>
2026-02-04 02:06:43 +0900

    ログイン機能の実装

--- src/index.php
+++ src/index.php
@@ -1,6 +1,18 @@
 <?php
-echo "HelloWorld!";
+echo $hallo = "HelloWorld!";
 
 $CurrentDate = date("Y-m-d H:i:s");
 echo "\nCurrent Date and Time: " . $CurrentDate;
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>トップページ</title>
+</head>
+<body>
+  <p><?php echo $hallo; ?> </p>
+  <p>現在の日時: <?php echo $CurrentDate; ?></p>
+  <p><a href="login.php">ログイン</a></p>
+</body>
+</html>
